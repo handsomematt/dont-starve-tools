9e4c7ddd42a80f2fe1269f19d6b2c19c99fcda24
davpsh <davps@bk.ru>
2016-07-19 18:08:03 +0600

    ProgressForm

---
+++ src/TEXTool/ProgressForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TEXTool
+{
+    public partial class ProgressForm : Form
+    {
+        public ProgressForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
